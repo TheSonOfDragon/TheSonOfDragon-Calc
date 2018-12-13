1fe10a766b5f53b8932cc9299bbb529fb48199f7
PIKK <17712030716@163.com>
2018-12-13 17:20:01 +0800

    提交界面

---
+++ 04.SourceCode/Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace Calculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        #region 标题栏所有事件
+        /// <summary>
+        /// 关闭窗口
+        /// </summary>
+        private void Btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// 窗口最小化
+        /// </summary>
+        private void Btn_min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// 窗口最大化
+        /// </summary>
+        private void Btn_max_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        /// <summary>
+        /// 窗口移动事件
+        /// </summary>
+        private void TitleBar_MouseMove(object sender,MouseEventArgs e)
+        {
+            if (e.LeftButton==MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        int i = 0;
+        /// <summary>
+        /// 鼠标双击事件
+        /// </summary>
+        private void TitleBar_MouseDown(object sender,MouseButtonEventArgs e)
+        {
+            i += 1;
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            timer.Tick += (s, e1) => { timer.IsEnabled = false; i = 0; };
+            timer.IsEnabled = true;
+            if (i % 2 == 0)
+            {
+                timer.IsEnabled = false;
+                i = 0;
+                this.WindowState = (this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized);
+            }
+        }
+
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.leftFlyout.Visibility = Visibility.Visible;
+            if (!this.leftFlyout.IsOpen)
+            {
+                this.leftFlyout.IsOpen = true;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.bottomFlyout.Visibility = Visibility.Visible;
+            if (!this.bottomFlyout.IsOpen)
+            {
+                this.bottomFlyout.IsOpen = true;
+            }
+        }
+    }
+}
