dd523df9566e64d96c8a1c2cd5789284c504ea29
zhoubao <13151537462@qq.com>
2018-12-14 16:04:07 +0800

    00

--- 04.SourceCode/WPFMVVMDemo-master/WPFMVVMDemo.ViewModel/FactoryNumber/NumberZero.cs
+++ 04.SourceCode/WPFMVVMDemo-master/WPFMVVMDemo.ViewModel/FactoryNumber/NumberZero.cs
@@ -1,35 +1,75 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
+
 using Memory;
+
 namespace WPFMVVMDemo.ViewModel.AddNumber
+
 {
-    
+
+
+
     class NumberZero : IJudge.JudgeZero
+
     {
+
         public string JudgeZero()
+
         {
+
             Cache.operatorCacheOld = Cache.operatorCacheNew;
+
             Cache.judgeTurn = true;
+
             Cache.judgeSinge = false;
+
             if (Cache.judgeNewInp)
+
             {
+
                 Cache.underCache = "";
+
                 Cache.judgeNewInp = false;
+
             }
+
             if (Cache.underCache == "")
+
             {
+
                 Cache.underCache = "0";
+
                 return Cache.underCache;
+
             }
-            else
+
+            if (Cache.underCache != "0")
+
             {
+
+
+
                 Cache.underCache += 0;
+
                 return Cache.underCache;
+
             }
+
+            return "";
+
+
+
         }
+
     }
-    
+
+
+
 }
