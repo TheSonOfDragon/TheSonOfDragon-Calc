bb2ce2620df314a03e8ff9de4c6ff9e82113585d
zhoubao <13151537462@qq.com>
2018-12-14 12:00:00 +0800

    decimal

--- 04.SourceCode/WPFMVVMDemo-master/Operation/ComplexOperation.cs
+++ 04.SourceCode/WPFMVVMDemo-master/Operation/ComplexOperation.cs
@@ -12,21 +12,31 @@ namespace Operation
 
         public string Sqrt(string num1)
         {
-            return Math.Sqrt(Convert.ToDouble(num1)).ToString();
+            decimal x = Convert.ToDecimal(num1) / 3;
+            decimal lastX = 0m;
+            for (int i = 0; i < 50; i++)
+            {
+                x = (Convert.ToDecimal(num1) / (x * 2)) + (x / 2);
+                if (x == lastX) break;
+                lastX = x;
+            }
+            return x.ToString();
+
+            //return Math.Sqrt(Convert.ToDouble(num1)).ToString();
         }
         public string Squ(string num1)
         {
-            return (Convert.ToDouble(num1) * Convert.ToDouble(num1)).ToString();
+            return (Convert.ToDecimal(num1) * Convert.ToDecimal(num1)).ToString();
         }
         public string OneCent(string num1)
         {
-            return (1 / Convert.ToDouble(num1)).ToString();
             
-            
+            return (1 / Convert.ToDecimal(num1)).ToString();
+   
         }
         public string Minus(string num1)
         {
-            return (-Convert.ToDouble(num1)).ToString();
+            return (-Convert.ToDecimal(num1)).ToString();
         }
     }
 }
